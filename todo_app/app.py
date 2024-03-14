2b1c14a823ac4c766d4a8f46e4f317c9e4ad7a16
Stuart Knowles <Stuart.Knowles@softwire.com>
2024-03-14 11:35:04 +0000

    Allow marking items as complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request, redirect
 from todo_app.flask_config import Config
 
 from .data import trello_items
+from .helpers import partition
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -10,8 +11,8 @@ app.config.from_object(Config())
 
 @app.route("/")
 def index():
-    items = trello_items.get_items()
-    return render_template("index.html", items=items)
+    to_do, done = partition(lambda x: x["status"] == "Done", trello_items.get_items())
+    return render_template("index.html", to_do_items=to_do, done_items=done)
 
 
 @app.post("/item")
@@ -20,5 +21,11 @@ def add_item():
     if submitted_title is None:
         app.logger.warning("item not provided")
     else:
-        trello_items.add_item(request.form.get("title"))
+        trello_items.add_item(submitted_title)
+    return redirect("/")
+
+
+@app.post("/complete_item/<id>")
+def complete_item(id):
+    trello_items.mark_item_done(id)
     return redirect("/")
--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,4 +1,3 @@
-import json
 import os
 
 import requests
@@ -9,26 +8,40 @@ HEADERS = {"Accept": "application/json"}
 
 BASE_QUERY = {"key": os.getenv("TRELLO_API_KEY"), "token": os.getenv("TRELLO_TOKEN")}
 
-LIST_ID = os.getenv("TRELLO_LIST_ID")
+BOARD_ID = os.environ["TRELLO_BOARD_ID"]
+
+
+list_names_to_id = {
+    list_["name"]: list_["id"]
+    for list_ in requests.get(
+        BASE_URL + f"boards/{BOARD_ID}/lists", headers=HEADERS, params=BASE_QUERY
+    ).json()
+}
+list_ids_to_name = {v: k for k, v in list_names_to_id.items()}
 
 
 def get_items():
     response = requests.get(
-        BASE_URL + f"lists/{LIST_ID}/cards",
+        BASE_URL + f"boards/{BOARD_ID}/cards",
         headers=HEADERS,
         params=BASE_QUERY,
     )
 
     return [
         {
-            "id": card["idShort"],
-            "status": "Complete" if card["closed"] else "Not Started",
+            "id": card["id"],
+            "status": list_ids_to_name[card["idList"]],
             "title": card["name"],
         }
-        for card in json.loads(response.text)
+        for card in response.json()
     ]
 
 
 def add_item(title):
-    query = {"idList": LIST_ID, "name": title}
+    query = {"idList": list_names_to_id["To do"], "name": title}
     requests.post(BASE_URL + "/cards", headers=HEADERS, params=BASE_QUERY | query)
+
+
+def mark_item_done(id):
+    query = {"idList": list_names_to_id["Done"]}
+    res = requests.put(BASE_URL + f"cards/{id}", headers=HEADERS, params=BASE_QUERY | query)
