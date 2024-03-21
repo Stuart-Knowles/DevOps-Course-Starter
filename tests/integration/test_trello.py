4a24c85395a471d95369f97237c29459dc91c78d
Stuart Knowles <Stuart.Knowles@softwire.com>
2024-03-21 16:29:48 +0000

    add extra assert

--- tests/integration/test_trello.py
+++ tests/integration/test_trello.py
@@ -8,17 +8,19 @@ from todo_app.src.trello.config import get_trello_config
 from .mock_trello_data import MOCK_LISTS, MOCK_ITEMS
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def mock_trello_api():
     base_url = f"https://api.trello.com/1/boards/{os.environ['TRELLO_BOARD_ID']}"
     with requests_mock.Mocker() as m:
         m.get(f"{base_url}/lists", json=MOCK_LISTS)
         m.get(f"{base_url}/cards", json=MOCK_ITEMS)
-        yield
+        yield m
 
 
-def test_index_page(test_client):
+def test_index_page(test_client, mock_trello_api):
     response = test_client.get("/")
-    assert response.status_code == 200
     html = response.data.decode()
+
+    assert response.status_code == 200
+    assert mock_trello_api.call_count == 2
     assert all([item["name"] in html for item in MOCK_ITEMS])
